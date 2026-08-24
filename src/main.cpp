f170dfe39218e07b36bff85fe578cf2e2c2c4306
Yestin Paraguya <yestinjohn.paraguya@g.msuiit.edu.ph>
2026-08-24 19:10:53 +0800

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+#define LED 2
+#define LED2 4
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  pinMode(LED2, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED, HIGH);
+  digitalWrite(LED2, LOW);
+  delay(500);
+  digitalWrite(LED, LOW);
+  digitalWrite(LED2, HIGH);
+  delay(500);
+}
